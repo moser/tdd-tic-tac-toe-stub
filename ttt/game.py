7e0439a74d628955c7ec28c740c2edf34db9b5e7
Martin Vielsmaier <martin@vielsmaier.net>
2019-11-19 15:21:39 +0100

    Add integration test idea.

--- ttt/game.py
+++ ttt/game.py
@@ -1,13 +1,22 @@
 
-def main():
+def main(stdin, stdout):
+
+    def prnt(msg, *args):
+        msg = msg + " " + repr(args)
+        stdout.write(msg + "\n")
+    def inpt(prompt):
+        stdout.write(prompt + " ")
+        return stdin.readline().strip()
+
     exit = False
     while not exit:
-        user_input = input("Ask player X for something: ")
-        print("Player entered:", user_input)
+        user_input = inpt("Ask player X for input:")
+        prnt("Player entered:", user_input)
         # TODO: Now do something with the input?
         if user_input == "exit":
             exit = True
 
 
 if __name__ == "__main__":
-    main()
+    import sys
+    main(sys.stdin, sys.stdout)
--- ttt/tests.py
+++ ttt/tests.py
@@ -2,3 +2,7 @@ import pytest
 
 def test_failing():
     assert False
+
+
+def test_integration():
+    pass
